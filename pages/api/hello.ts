a70a89108ea5d7170f72f77e4c62b08f637c1ec3
Dt22 <sindistant@gmail.com>
2023-01-19 01:37:42 +0800

    first commit

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -11,3 +11,22 @@ export default function handler(
 ) {
   res.status(200).json({ name: 'John Doe' })
 }
+
+// import '../styles/globals.css'
+// import Header from './Header'
+
+// export default function RootLayout({
+//     children,
+// }: {
+//     children: React.ReactNode
+// }) {
+//     return (
+//         <html>
+//             <head></head>
+//             <body>
+//                 <Header/>
+//                 {children}
+//             </body>
+//         </html>
+//     )
+// }
